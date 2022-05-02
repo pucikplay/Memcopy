097290b25b69d2259def2cf6f3960752e72d5706
pucikplay <pucikplay@gmail.com>
2022-05-02 16:09:01 +0200

    Driver program written

--- app/main.c
+++ app/main.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#include "memcopy.h"
+
+int main(void)
+{
+    printf("This is a driver program for memcopy\n");
+
+    char* s1 = "Some string";
+    char* s2 = malloc(strlen(s1) + 1);
+
+    memcopy_uint64(s2, s1, strlen(s1) + 1);
+
+    printf("s1: %s, s2: %s\n", s1, s2);
+
+    free(s2);
+}
